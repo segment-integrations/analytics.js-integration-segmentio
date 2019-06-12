961ee11b2e8323e431f069b13454ced52d29f6e7
Prateek Srivastava <f2prateek@gmail.com>
2019-06-12 14:15:43 -0700

    build: update tooling
    This updates a variety of tooling we use to build and releaase the library, such as:
    * Update to circle ci v2 as v1 is deprecated
    * Use commitlint and commitizen to maintain clean commit history
    * Update Karma configuration so we can use it with codecov to report coverage (see https://github.com/segmentio/analytics.js-core/pull/75 for notes on implementation)
    * Use np to simplify the releasing process

--- karma.conf.ci.js
+++ karma.conf.ci.js
@@ -33,23 +33,11 @@ var customLaunchers = {
     browserName: 'safari',
     version: '9.0'
   },
-  // FIXME(ndhoule): Bad IE7/8 support in testing packages make these fail
-  // sl_ie_7: {
-  //   base: 'SauceLabs',
-  //   browserName: 'internet explorer',
-  //   version: '7'
-  // },
-  // sl_ie_8: {
-  //   base: 'SauceLabs',
-  //   browserName: 'internet explorer',
-  //   version: '8'
-  // },
-  // FIXME(): IE9 tests are failing pre-Browserification
-  // sl_ie_9: {
-  //   base: 'SauceLabs',
-  //   browserName: 'internet explorer',
-  //   version: '9'
-  // },
+  sl_ie_9: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer',
+    version: '9'
+  },
   sl_ie_10: {
     base: 'SauceLabs',
     browserName: 'internet explorer',
@@ -70,35 +58,41 @@ module.exports = function(config) {
   baseConfig(config);
 
   if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-    throw new Error('SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are required but are missing');
+    throw new Error(
+      'SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are required but are missing'
+    );
   }
 
   config.set({
-    browserDisconnectTolerance: 2,
-    browserDisconnectTimeout: 30000,
-    browserNoActivityTimeout: 30000,
+    browserDisconnectTolerance: 1,
+
+    browserDisconnectTimeout: 60000,
+
+    browserNoActivityTimeout: 60000,
 
     singleRun: true,
 
-    reporters: ['progress', 'junit', 'coverage'],
+    concurrency: 2,
+
+    retryLimit: 5,
+
+    reporters: ['spec', 'summary', 'junit'],
+
+    specReporter: {
+      suppressPassed: true
+    },
+
+    junitReporter: {
+      outputDir: 'junit-reports',
+      suite: require('./package.json').name
+    },
 
     browsers: ['PhantomJS'].concat(Object.keys(customLaunchers)),
 
     customLaunchers: customLaunchers,
 
-    junitReporter: {
-      outputDir: process.env.TEST_REPORTS_DIR,
-      suite: require('./package.json').name
-    },
-
     sauceLabs: {
       testName: require('./package.json').name
-    },
-
-    coverageReporter: {
-      reporters: [
-        { type: 'lcov' }
-      ]
     }
   });
 };
